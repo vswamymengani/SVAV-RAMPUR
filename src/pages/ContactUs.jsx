b96d71780d514451b925641ada2b10502f4c1190
divyaNarasimhuludivya <130662887+vuyyalaarchana123@users.noreply.github.com>
2024-08-08 14:07:39 +0530

    Updated contactus screen

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -46,17 +46,31 @@ const ContactUs = () => {
   return (
     <div>
     <h1 id="i1">Contact Us:</h1><br></br>
-    <div id="i2"></div>
+    <div id="i2">
+    <iframe
+  src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d120821.03486408918!2d79.21146627448185!3d18.857897875716414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x3bcd35e11af0283d%3A0x69277e2f4d319e41!2sV75V%2B5G9%2C%20Subbapally%2C%20Telangana%20504207!3m2!1d18.857916!2d79.293868!5e0!3m2!1sen!2sin!4v1722970369274!5m2!1sen!2sin"
+  width="90%"
+  height="100%"
+  style={{ border: 0 }}
+  allowFullScreen=""
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+  title="School Location Map"
+></iframe>
+
+       </div>
     <div>
       <h2 id="i3">
         Write To Us On:<br></br><br></br>
-        <h5 id="i4">Registered Address:</h5><br></br>
+        <h4 id="i7">&nbsp;&nbsp;&nbsp;svavrampus.com</h4><br></br>
+        <h5 id="i4">Central Office:</h5><br></br>
+       
         <h4 id="i7">
-       &nbsp; Sri Vidyaranya Avasa Vidyalayam,<br></br>
-       &nbsp; Chokkaram Nagar, Rampur,<br></br>
-       &nbsp; Hazipur Mondal, Mancherial district<br></br>
-       &nbsp; Telangana state -500301<br></br>
-       &nbsp; Phone: 087362 32024
+       &nbsp; Vidyapeetham,Sree Sharadadhamam,<br></br>
+       &nbsp; KismatpurRd, Abhyudaya Nagar,<br></br>
+       &nbsp; Bandlaguda Jagir, Hyderabad,<br></br>
+       &nbsp; Telangana-500086<br></br>
+       &nbsp; Phone: 095538 88801
 
      </h4>
       </h2>
